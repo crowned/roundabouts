486f7bab07a7261ce282e4b7ee485c4c20d29f1f
Nick G <nick@Verizon-Firewall.local>
2014-03-19 12:36:07 +0100

    transform the box

--- js/script.js
+++ js/script.js
@@ -12,14 +12,16 @@ $('document').ready(function() {
 
 	//scale pairs
 	scale= 75 / 100;
-	
-	$(".elem.A").css("zoom", scale);
-	$(".elem.B").css("zoom", 1-scale);
+	alert("fuck");
+    $(".box.A").css("transform", "scale("+scale+","+scale+")");
+    $(".box.B").css("transform", "scale("+(1-scale)+","+(1-scale)+")");
+
 
 	$("#slider-step").on("change", function(e){
 	    scale= $("#slider-step").val() / 100;
 
-	    $(".elem.A").css("zoom", scale);
-	    $(".elem.B").css("zoom", 1-scale);
+	    $(".box.A").css("transform", "scale("+scale+","+scale+")");
+	    $(".box.B").css("transform", "scale("+(1-scale)+","+(1-scale)+")");
+
 	});
 });
