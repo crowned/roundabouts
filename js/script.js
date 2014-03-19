6147b82a9d8a813353e86d2cff79625135396743
Nick G <nick@Verizon-Firewall.local>
2014-03-19 13:44:50 +0100

    working in firefox

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $('document').ready(function() {   
+	var updateZoom = function(scale) {
+		var oppositeScale = 1 - scale;
+		boxA.attr("style","transform:scale3d("+scale+","+scale+", "+scale+");-webkit-transform:scale3d("+scale+","+scale+", "+scale+")");
+		boxB.attr("style","transform:scale3d("+oppositeScale+","+oppositeScale+", "+oppositeScale+");-webkit-transform:scale3d("+oppositeScale+","+oppositeScale+", "+oppositeScale+")");
+		// boxA.css("transform", "scale("+scale+","+scale+")");
+		// boxB.css("transform", "scale("+(1-scale)+","+(1-scale)+")");
+	};
+
 	//determine number of spiral pairs
-	vCount = Math.ceil(1.35*$(window).height() / $("li").first().height());
+	vCount = Math.ceil(1.55*$(window).height() / $("li").first().height());
 	hCount = Math.ceil(2.75*$(window).width() / $("li").first().width());
 	numberOfBlocks = vCount * hCount;
 	//populate page with li elements
@@ -8,18 +16,18 @@ $('document').ready(function() {
 		{ 
 			$("li").last().clone().appendTo("ul");
 		};
+		var boxA = $(".box.A");
+		var boxB = $(".box.B");
+
+
 
 	//scale pairs
 	scale= 75 / 100;
-    $(".box.A").css("transform", "scale("+scale+","+scale+")");
-    $(".box.B").css("transform", "scale("+(1-scale)+","+(1-scale)+")");
-
+    boxA.css("transform", "scale("+scale+","+scale+")");
+	boxB.css("transform", "scale("+(1-scale)+","+(1-scale)+")");
 
 	$("#slider-step").on("change", function(e){
-	    scale= $("#slider-step").val() / 100;
-
-	    $(".box.A").css("transform", "scale("+scale+","+scale+")");
-	    $(".box.B").css("transform", "scale("+(1-scale)+","+(1-scale)+")");
-
+		scale= $("#slider-step").val() / 100;
+    	updateZoom(scale); 
 	});
 });
